36bc09d56c319d6ab21f372d496e096012a9c587
tiesiogfoto <tiesiogfoto@gmail.com>
2025-09-15 20:53:34 +0200

    Update layout.tsx

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,7 +1,8 @@
 import './globals.css';
 import type { Metadata } from 'next';
-import Navbar from '@/components/Navbar';
-import Footer from '@/components/Footer';
+import Navbar from '../components/Navbar';
+import Footer from '../components/Footer';
+
 
 
 export const metadata: Metadata = {
