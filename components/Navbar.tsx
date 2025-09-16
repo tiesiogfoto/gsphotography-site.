07bc46327406676559253c48a3de04f3e1a8f6c7
tiesiogfoto <tiesiogfoto@gmail.com>
2025-09-16 11:42:25 +0200

    Update Navbar.tsx

--- components/Navbar.tsx
+++ components/Navbar.tsx
@@ -4,22 +4,18 @@ export default function Navbar() {
   return (
     <nav className="bg-white shadow-md p-4 sticky top-0 z-50">
       <div className="max-w-6xl mx-auto flex items-center justify-between">
-        {/* Kairėje logotipas */}
         <div className="text-xl font-bold text-gray-800">GSphotography</div>
 
-        {/* Meniu dešinėje */}
         <div className="space-x-6 text-gray-700 font-medium flex items-center">
           <Link href="/">Home</Link>
           <Link href="/portfolio">Portfolio</Link>
           <Link href="/about">About</Link>
           <Link href="/contact">Contact</Link>
 
-          {/* Telefonas */}
+          {/* Telefonas ir el. paštas – be ikonų */}
           <a href="tel:+4746262381" className="hover:text-black">
             46 26 23 81
           </a>
-
-          {/* El. paštas */}
           <a href="mailto:hello@gsphotography.no" className="hover:text-black">
             hello@gsphotography.no
           </a>
@@ -28,4 +24,3 @@ export default function Navbar() {
     </nav>
   );
 }
-
