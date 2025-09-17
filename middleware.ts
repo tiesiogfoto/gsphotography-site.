e02542a53996e306648b6d0c96c83e97486cdbb7
tiesiogfoto <tiesiogfoto@gmail.com>
2025-09-17 22:26:21 +0200

    Update middleware.ts

--- middleware.ts
+++ middleware.ts
@@ -1,10 +1,8 @@
-// middleware.ts
 import createMiddleware from 'next-intl/middleware';
 import intlConfig from './next-intl.config';
 
 export default createMiddleware(intlConfig);
 
 export const config = {
-  // aptarnaujam šaknį ir lokalizuotus path’us
   matcher: ['/', '/(en|no|lt)/:path*']
 };
