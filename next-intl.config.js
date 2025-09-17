6126b059f55bdcf775696759058b62621dfb9d2a
tiesiogfoto <tiesiogfoto@gmail.com>
2025-09-17 21:54:01 +0200

    Update and rename next-intl.config.ts to next-intl.config.js

---
+++ next-intl.config.js
@@ -0,0 +1,5 @@
+// next-intl.config.js
+module.exports = {
+  locales: ["en", "no", "lt"],
+  defaultLocale: "no"
+};
--- next-intl.config.ts
+++
@@ -1,7 +0,0 @@
-// next-intl.config.ts
-const intlConfig = {
-  locales: ["en", "no", "lt"],
-  defaultLocale: "no"
-};
-
-export default intlConfig;
